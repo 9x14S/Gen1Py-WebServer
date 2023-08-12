dd24071e69aac1a4f03d5838319390ef9882ae72
Micah Raney <micah-raney@users.noreply.github.com>
2023-08-12 14:52:13 +1200

    add route for edit_page and add test values

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ app = Flask(__name__) # Turn this file into a Flask app
 @app.route("/")
 def index():
 	return render_template("index.html")
+
+@app.route("/edit")
+def edit():
+	return render_template("edit_page.html", playername="test_name", money="test_money", playerid=-1, firstpokename="test_pokename")

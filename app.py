eacc49617a96265beab54242a9fc3da28bae1da0
Micah Raney <micah-raney@users.noreply.github.com>
2023-08-12 14:48:50 +1200

    add index.html for a better homepage.

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app = Flask(__name__) # Turn this file into a Flask app
 # Python Decorator assigns the below function to the assigned route.
 @app.route("/")
 def index():
-	return render_template("edit_page.html")
+	return render_template("index.html")
